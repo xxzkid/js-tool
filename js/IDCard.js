d4afd58ac98cbe98ddb4808bc150b342c2efc132
xxzkid <xxzkid@sina.com>
2015-11-04 18:58:11 +0800

    修改了访问方式
    Signed-off-by: xxzkid

--- js/IDCard.js
+++ js/IDCard.js
@@ -1,20 +1,21 @@
 (function() {
-	var vcity = {
-		11: "北京", 12: "天津", 13: "河北", 14: "山西", 15: "内蒙古",
-		21: "辽宁", 22: "吉林", 23: "黑龙江",
-		31: "上海", 32: "江苏", 33: "浙江", 34: "安徽", 35: "福建", 36: "江西", 37: "山东",
-		41: "河南", 42: "湖北", 43: "湖南", 44: "广东", 45: "广西", 46: "海南",
-		50: "重庆", 51: "四川", 52: "贵州", 53: "云南", 54: "西藏", 
-		61: "陕西", 62: "甘肃", 63: "青海", 64: "宁夏", 65: "新疆",
-		71: "台湾",
-		81: "香港", 82: "澳门",
-		91: "国外"
-	};
 
-	window.IDCard = {
-
+	window.IDCard = (function(){
+		
+		var vcity = {
+			11: "北京", 12: "天津", 13: "河北", 14: "山西", 15: "内蒙古",
+			21: "辽宁", 22: "吉林", 23: "黑龙江",
+			31: "上海", 32: "江苏", 33: "浙江", 34: "安徽", 35: "福建", 36: "江西", 37: "山东",
+			41: "河南", 42: "湖北", 43: "湖南", 44: "广东", 45: "广西", 46: "海南",
+			50: "重庆", 51: "四川", 52: "贵州", 53: "云南", 54: "西藏", 
+			61: "陕西", 62: "甘肃", 63: "青海", 64: "宁夏", 65: "新疆",
+			71: "台湾",
+			81: "香港", 82: "澳门",
+			91: "国外"
+		};
+		
 		//检查号码是否符合规范，包括长度，类型  
-		isCardNo: function(card) {
+		function isCardNo(card) {
 			//身份证号码为15位或者18位，15位时全为数字，18位前17位为数字，最后一位是校验位，可能为数字或字符X  
 			var reg = /(^\d{15}$)|(^\d{17}(\d|X)$)/;
 			if (reg.test(card) === false) {
@@ -22,19 +23,19 @@
 			}
 
 			return true;
-		},
-
+		};
+		
 		//取身份证前两位,校验省份  
-		checkProvince: function(card) {
+		function checkProvince(card) {
 			var province = card.substr(0, 2);
 			if (vcity[province] == undefined) {
 				return false;
 			}
 			return true;
-		},
-
+		};
+		
 		//检查生日是否正确  
-		checkBirthday: function(card) {
+		function checkBirthday(card) {
 			var len = card.length;
 			//身份证15位时，次序为省（3位）市（3位）年（2位）月（2位）日（2位）校验位（3位），皆为数字  
 			if (len == '15') {
@@ -44,7 +45,7 @@
 				var month = arr_data[3];
 				var day = arr_data[4];
 				var birthday = new Date('19' + year + '/' + month + '/' + day);
-				return this.verifyBirthday('19' + year, month, day, birthday);
+				return verifyBirthday('19' + year, month, day, birthday);
 			}
 			//身份证18位时，次序为省（3位）市（3位）年（4位）月（2位）日（2位）校验位（4位），校验位末尾可能为X  
 			if (len == '18') {
@@ -54,13 +55,13 @@
 				var month = arr_data[3];
 				var day = arr_data[4];
 				var birthday = new Date(year + '/' + month + '/' + day);
-				return this.verifyBirthday(year, month, day, birthday);
+				return verifyBirthday(year, month, day, birthday);
 			}
 			return false;
-		},
-
+		};
+		
 		//校验日期  
-		verifyBirthday: function(year, month, day, birthday) {
+		function verifyBirthday(year, month, day, birthday) {
 			var now = new Date();
 			var now_year = now.getFullYear();
 			birthday = new Date(birthday);
@@ -74,12 +75,12 @@
 				return false;
 			}
 			return false;
-		},
-
+		};
+		
 		//校验位的检测  
-		checkParity: function(card) {
+		function checkParity(card) {
 			//15位转18位  
-			card = this.changeFivteenToEighteen(card);
+			card = changeFivteenToEighteen(card);
 			var len = card.length;
 			if (len == '18') {
 				var arrInt = new Array(7, 9, 10, 5, 8, 4, 2, 1, 6, 3, 7, 9, 10, 5, 8, 4, 2);
@@ -96,10 +97,10 @@
 				return false;
 			}
 			return false;
-		},
-
+		};
+		
 		//15位转18位身份证号  
-		changeFivteenToEighteen: function(card) {
+		function changeFivteenToEighteen(card) {
 			if (card.length == '15') {
 				var arrInt = new Array(7, 9, 10, 5, 8, 4, 2, 1, 6, 3, 7, 9, 10, 5, 8, 4, 2);
 				var arrCh = new Array('1', '0', 'X', '9', '8', '7', '6', '5', '4', '3', '2');
@@ -113,20 +114,22 @@
 				return card;
 			}
 			return card;
-		},
-
+		};
+		
 		/**
 		 * 给外部调用的方法 var b = IDCard.checkIDCard(card);
 		 */
-		checkIDCard: function(card) {
-			var b = this.isCardNo(card);
+		function checkIDCard(card) {
+			var b = isCardNo(card);
 			if (!b) return b;
-			b = this.checkProvince(card);
+			b = checkProvince(card);
 			if (!b) return b;
-			b = this.checkBirthday(card);
+			b = checkBirthday(card);
 			if (!b) return b;
-			b = this.checkParity(card);
+			b = checkParity(card);
 			return b;
-		}
-	}
+		};
+		
+		return {checkIDCard : checkIDCard};
+	})();
 })();
--- js/msg.js
+++ js/msg.js
@@ -3,186 +3,195 @@
  * 时间：2015-10-29
  * 描述：消息框
  */
-var msg = {
-
-	rect: function() {
-		var width = Math.max(document.body.scrollWidth, document.documentElement.scrollWidth);
-		var height = Math.max(document.body.scrollHeight, document.documentElement.scrollHeight);
-		var w = 280;
-		var h = 100;
-		var left = width / 2 - w / 2;
-		var top = height / 2 - h / 2;
-		var zIndex = 999;
-		var color = "#fff";
-		return { "width": width, "height": height, "left": left, "top": top, "w": w, "h": h, "zIndex": zIndex, "color": color };
-	},
-
-	alert: function(title, msg) {
-		var rect = this.rect();
-		this.openlayer();
-		var div = document.createElement("div");
-		div.id = "xxxxalert";
-		div.style.position = "fixed";
-		div.style.left = rect.left + "px";
-		div.style.top = rect.top + "px";
-		div.style.width = rect.w + "px";
-		div.style.height = rect.h + "px";
-		div.style.zIndex = rect.zIndex;
-		div.style.background = rect.color;
+(function(){
+	window.msg = (function(){
 		
-		var hg = 20;
-		var marginRight = 10;
-		var marginLeft = 10;
-		var topdiv = document.createElement("div");
-		div.appendChild(topdiv);
-		topdiv.style.height = hg + "px";
+		var rect = function() {
+			var width = Math.max(document.body.scrollWidth, document.documentElement.scrollWidth);
+			var height = Math.max(document.body.scrollHeight, document.documentElement.scrollHeight);
+			var w = 280;
+			var h = 100;
+			var left = width / 2 - w / 2;
+			var top = height / 2 - h / 2;
+			var zIndex = 999;
+			var color = "#fff";
+			return { "width": width, "height": height, "left": left, "top": top, "w": w, "h": h, "zIndex": zIndex, "color": color };
+		};
 		
-		var titlep = document.createElement("p");
-		topdiv.appendChild(titlep);
-		titlep.innerText = title;
-		titlep.style.float = "left";
-		titlep.style.width = rect.w / 2 - marginLeft + "px";
-		titlep.style.height = hg + "px";
-		titlep.style.marginTop = "0px";
-		titlep.style.marginLeft = marginLeft + "px";
-		titlep.style.lineHeight = hg + "px";
-
-		var closep = document.createElement("p");
-		topdiv.appendChild(closep);
-
-		closep.style.cursor = "pointer";
-		closep.style.float = "right";
-		closep.style.marginTop = "0px";
-		closep.style.marginRight = marginRight + "px";
-		closep.style.width = rect.w / 2 - marginRight + "px";
-		closep.style.height = hg + "px";
-		closep.style.textAlign = "right";
-		closep.style.lineHeight = hg + "px";
-		closep.innerText = "x";
-		closep.addEventListener("click", function() {
-			document.getElementById("xxxxlayer").remove();
-			document.getElementById("xxxxalert").remove();
-		});
-
-		var centerdiv = document.createElement("div");
-		div.appendChild(centerdiv);
-
-		var txtp = document.createElement("p");
-		centerdiv.appendChild(txtp);
-		txtp.innerText = msg;
-		txtp.style.textAlign = "center";
-		txtp.style.width = rect.w + "px";
-		txtp.style.height = rect.h - hg + "px";
-
-		document.body.appendChild(div);
-	},
-
-	confirm: function(title, msg, callback) {
-		this.openlayer();
-		var rect = this.rect();
-		var div = document.createElement("div");
-		div.id = "xxxxconfirm";
-		div.style.position = "fixed";
-		div.style.width = rect.w + "px";
-		div.style.height = rect.h + "px";
-		div.style.left = rect.left + "px";
-		div.style.top = rect.top + "px";
-		div.style.zIndex = rect.zIndex;
-		div.style.background = rect.color;
+		var openlayer = function() {
+			var div = document.createElement("div");
+			div.id = "xxxxlayer";
+			var r = rect();
+			console.log(r);
+			div.style.width = r.width + "px";
+			div.style.height = r.height + "px";
+			div.style.position = "absolute";
+			div.style.left = "0px";
+			div.style.top = "0px";
+			div.style.zIndex = 998;
+			div.style.background = "#000";
+			div.style.filter = "alpha(opacity=10)";
+			div.style.opacity = "0.20";
+			document.body.appendChild(div);
+		};
 		
-		var hg = 20;
-		var marginRight = 10;
-		var marginLeft = 10;
-		var topdiv = document.createElement("div");
-		div.appendChild(topdiv);
-		topdiv.style.width = rect.w + "px";
-		topdiv.style.height = hg + "px";
+		function alert(title, msg) {
+			openlayer();
+			var r = rect();
+			var div = document.createElement("div");
+			div.id = "xxxxalert";
+			div.style.position = "fixed";
+			div.style.left = r.left + "px";
+			div.style.top = r.top + "px";
+			div.style.width = r.w + "px";
+			div.style.height = r.h + "px";
+			div.style.zIndex = r.zIndex;
+			div.style.background = r.color;
+			
+			var hg = 20;
+			var marginRight = 10;
+			var marginLeft = 10;
+			var topdiv = document.createElement("div");
+			div.appendChild(topdiv);
+			topdiv.style.height = hg + "px";
+			
+			var titlep = document.createElement("p");
+			topdiv.appendChild(titlep);
+			titlep.innerText = title;
+			titlep.style.float = "left";
+			titlep.style.width = r.w / 2 - marginLeft + "px";
+			titlep.style.height = hg + "px";
+			titlep.style.marginTop = "0px";
+			titlep.style.marginLeft = marginLeft + "px";
+			titlep.style.lineHeight = hg + "px";
+	
+			var closep = document.createElement("p");
+			topdiv.appendChild(closep);
+	
+			closep.style.cursor = "pointer";
+			closep.style.float = "right";
+			closep.style.marginTop = "0px";
+			closep.style.marginRight = marginRight + "px";
+			closep.style.width = r.w / 2 - marginRight + "px";
+			closep.style.height = hg + "px";
+			closep.style.textAlign = "right";
+			closep.style.lineHeight = hg + "px";
+			closep.innerText = "x";
+			closep.addEventListener("click", function() {
+				document.getElementById("xxxxlayer").remove();
+				document.getElementById("xxxxalert").remove();
+			});
+	
+			var centerdiv = document.createElement("div");
+			div.appendChild(centerdiv);
+	
+			var txtp = document.createElement("p");
+			centerdiv.appendChild(txtp);
+			txtp.innerText = msg;
+			txtp.style.textAlign = "center";
+			txtp.style.width = r.w + "px";
+			txtp.style.height = r.h - hg + "px";
+	
+			document.body.appendChild(div);
+		}
 		
-		var titlep = document.createElement("p");
-		topdiv.appendChild(titlep);
-		titlep.style.width = rect.w / 2 - marginLeft + "px";
-		titlep.style.height = hg + "px";
-		titlep.style.float = "left";
-		titlep.style.marginLeft = marginLeft + "px";
-		titlep.style.lineHeight = hg + "px";
-		titlep.style.marginTop = "0px";
-		titlep.innerText = title;
+		function confirm(title, msg, callback) {
+			openlayer();
+			var r = rect();
+			var div = document.createElement("div");
+			div.id = "xxxxconfirm";
+			div.style.position = "fixed";
+			div.style.width = r.w + "px";
+			div.style.height = r.h + "px";
+			div.style.left = r.left + "px";
+			div.style.top = r.top + "px";
+			div.style.zIndex = r.zIndex;
+			div.style.background = r.color;
+			
+			var hg = 20;
+			var marginRight = 10;
+			var marginLeft = 10;
+			var topdiv = document.createElement("div");
+			div.appendChild(topdiv);
+			topdiv.style.width = r.w + "px";
+			topdiv.style.height = hg + "px";
+			
+			var titlep = document.createElement("p");
+			topdiv.appendChild(titlep);
+			titlep.style.width = r.w / 2 - marginLeft + "px";
+			titlep.style.height = hg + "px";
+			titlep.style.float = "left";
+			titlep.style.marginLeft = marginLeft + "px";
+			titlep.style.lineHeight = hg + "px";
+			titlep.style.marginTop = "0px";
+			titlep.innerText = title;
+			
+			var closep = document.createElement("p");
+			topdiv.appendChild(closep);
+			closep.style.width = r.w / 2 - marginRight + "px";
+			closep.style.height = hg + "px";
+			closep.style.textAlign = "right";
+			closep.style.float = "right";
+			closep.style.marginTop = "0px";
+			closep.style.cursor = "pointer";
+			closep.style.marginRight = marginRight + "px";
+			closep.style.lineHeight = hg + "px";
+			closep.innerText = "x";
+			closep.addEventListener("click", function(){
+				document.getElementById("xxxxlayer").remove();
+				document.getElementById("xxxxconfirm").remove();
+			});
+			
+			var centerdiv = document.createElement("div");
+			div.appendChild(centerdiv);
+			centerdiv.style.height = r.h - (hg * 2) + "px";
+			centerdiv.style.textAlign = "center";
+			centerdiv.innerText = msg;
+			
+	
+			var bottomdiv = document.createElement("div");
+			div.appendChild(bottomdiv);
+			bottomdiv.style.height = hg + "px";
+			
+			var okdiv = document.createElement("div");
+			bottomdiv.appendChild(okdiv);
+			okdiv.style.width = r.w / 2 + "px";
+			okdiv.style.height = hg + "px";
+			okdiv.style.float = "left";
+			okdiv.style.textAlign = "center";
+			okdiv.style.cursor = "pointer";
+			okdiv.innerText = "确定";
+			okdiv.addEventListener("click", function(){
+				document.getElementById("xxxxlayer").remove();
+				document.getElementById("xxxxconfirm").remove();
+				if(typeof(callback) === 'function'){
+					callback(true);
+				}
+			});
+			
+			var canceldiv = document.createElement("div");
+			bottomdiv.appendChild(canceldiv);
+			canceldiv.style.width = r.w / 2 + "px";
+			canceldiv.style.height = r.h + "px";
+			canceldiv.style.float = "left";
+			canceldiv.style.textAlign = "center";
+			canceldiv.style.cursor = "pointer";
+			canceldiv.innerText = "取消";
+			canceldiv.addEventListener("click", function(){
+				document.getElementById("xxxxlayer").remove();
+				document.getElementById("xxxxconfirm").remove();
+				if(typeof(callback) === 'function'){
+					callback(false);
+				}
+			});
+	
+			document.body.appendChild(div);
+		}
 		
-		var closep = document.createElement("p");
-		topdiv.appendChild(closep);
-		closep.style.width = rect.w / 2 - marginRight + "px";
-		closep.style.height = hg + "px";
-		closep.style.textAlign = "right";
-		closep.style.float = "right";
-		closep.style.marginTop = "0px";
-		closep.style.cursor = "pointer";
-		closep.style.marginRight = marginRight + "px";
-		closep.style.lineHeight = hg + "px";
-		closep.innerText = "x";
-		closep.addEventListener("click", function(){
-			document.getElementById("xxxxlayer").remove();
-			document.getElementById("xxxxconfirm").remove();
-		});
-		
-		var centerdiv = document.createElement("div");
-		div.appendChild(centerdiv);
-		centerdiv.style.height = rect.h - (hg * 2) + "px";
-		centerdiv.style.textAlign = "center";
-		centerdiv.innerText = msg;
-		
-
-		var bottomdiv = document.createElement("div");
-		div.appendChild(bottomdiv);
-		bottomdiv.style.height = hg + "px";
-		
-		var okdiv = document.createElement("div");
-		bottomdiv.appendChild(okdiv);
-		okdiv.style.width = rect.w / 2 + "px";
-		okdiv.style.height = hg + "px";
-		okdiv.style.float = "left";
-		okdiv.style.textAlign = "center";
-		okdiv.style.cursor = "pointer";
-		okdiv.innerText = "确定";
-		okdiv.addEventListener("click", function(){
-			document.getElementById("xxxxlayer").remove();
-			document.getElementById("xxxxconfirm").remove();
-			if(typeof(callback) === 'function'){
-				callback(true);
-			}
-		});
-		
-		var canceldiv = document.createElement("div");
-		bottomdiv.appendChild(canceldiv);
-		canceldiv.style.width = rect.w / 2 + "px";
-		canceldiv.style.height = rect.h + "px";
-		canceldiv.style.float = "left";
-		canceldiv.style.textAlign = "center";
-		canceldiv.style.cursor = "pointer";
-		canceldiv.innerText = "取消";
-		canceldiv.addEventListener("click", function(){
-			document.getElementById("xxxxlayer").remove();
-			document.getElementById("xxxxconfirm").remove();
-			if(typeof(callback) === 'function'){
-				callback(false);
-			}
-		});
-
-		document.body.appendChild(div);
-	},
-
-	openlayer: function openlayer() {
-		var div = document.createElement("div");
-		div.id = "xxxxlayer";
-		div.style.width = this.rect().width + "px";
-		div.style.height = this.rect().height + "px";
-		div.style.position = "absolute";
-		div.style.left = "0px";
-		div.style.top = "0px";
-		div.style.zIndex = 998;
-		div.style.background = "#000";
-		div.style.filter = "alpha(opacity=10)";
-		div.style.opacity = "0.20";
-		document.body.appendChild(div);
-	}
-
-};
+		return {
+			alert : alert,
+			confirm : confirm
+		};
+	})();
+	
+})();
